ddf77c8323a516627c17e4e8bd4ec965bf4b71c6
kswr <ksawery.oleszynski@gmail.com>
2018-03-03 22:44:19 +0100

    reformated

--- 001_Name_generator/app.py
+++ 001_Name_generator/app.py
@@ -4,16 +4,16 @@ import random as rd
 letters = string.ascii_lowercase
 let = len(letters)
 
-def nameGenerator(number_of_names, min_len = 5, max_len = 10):
+
+def nameGenerator(number_of_names, min_len=5, max_len=10):
     """This function returns list of random names"""
     names = []
     for i in range(0, number_of_names):
         name = ""
-        for i in range(0, rd.randint(min_len, max_len+1)):
-            name += letters[rd.randint(0,let-1)]
+        for i in range(0, rd.randint(min_len, max_len + 1)):
+            name += letters[rd.randint(0, let - 1)]
         names.append(name.title())
     return names
 
 
 print(nameGenerator(12, 4, 15))
-
